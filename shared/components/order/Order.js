b1ca8cee10368407d775d7e2735fdfedacf096ef
pepper <yoursilverdiamond@gmail.com>
2025-04-25 09:35:31 +0900

    주문 관리 / 더미 완료

--- shared/components/order/Order.js
+++ shared/components/order/Order.js
@@ -14,6 +14,33 @@ import { PrimaryButton } from '@/shared/layout-components/styles/button';
 import OrderTable from './OrderTable';
 import { washKeys } from '@/shared/functions/keys';
 
+const deliveryStatus = [
+  { value: '', label: '전체' },
+  { value: '자체 수거', label: '자체 수거' },
+  { value: '배송 준비', label: '배송 준비' },
+  { value: '배송중', label: '배송중' },
+  { value: '배송 완료', label: '배송 완료' },
+];
+
+const returnStatus = [
+  { value: '', label: '전체' },
+  { value: '완료', label: '완료' },
+  { value: '미완료', label: '미완료' },
+];
+
+const companyList = [
+  { value: '서울대학교병원', label: '서울대학교병원' },
+  { value: '롯데호텔 서울', label: '롯데호텔 서울' },
+  { value: '아워홈 구내식당', label: '아워홈 구내식당' },
+  { value: '현대제철 당진공장', label: '현대제철 당진공장' },
+  { value: '웨스틴조선호텔', label: '웨스틴조선호텔' },
+  { value: '삼성서울병원', label: '삼성서울병원' },
+  { value: '신세계푸드 본사', label: '신세계푸드 본사' },
+  { value: 'LG화학 여수공장', label: 'LG화학 여수공장' },
+  { value: '그랜드하얏트호텔', label: '그랜드하얏트호텔' },
+  { value: '고려대학교병원', label: '고려대학교병원' },
+];
+
 const Order = () => {
   const searchType = [
     { value: '1', label: '세탁 주문 번호' },
@@ -33,133 +60,133 @@ const Order = () => {
 
   const [data, setData] = useState([
     {
-      [washKeys.requestUid]: '143143141',
-      [washKeys.requestDate]: '2021-08-31',
-      [washKeys.productUid]: '123123',
-      [washKeys.productName]: '품목명',
-      [washKeys.requestQuantity]: 3,
-      [washKeys.estimatedDate]: '2021-09-01',
-      [washKeys.company]: '요청 업체',
-      [washKeys.deliveryStatus]: '배송 상태',
-      [washKeys.sendDate]: '2021-09-01',
-      [washKeys.washStatus]: '세탁 대기',
+      [washKeys.requestUid]: 'W202501130001',
+      [washKeys.requestDate]: '2025-04-20',
+      [washKeys.productUid]: 'P00000001',
+      [washKeys.productName]: '의료용 가운',
+      [washKeys.requestQuantity]: 98,
+      [washKeys.estimatedDate]: '2025-04-23',
+      [washKeys.company]: '서울대학교병원',
+      [washKeys.deliveryStatus]: '배송 완료',
+      [washKeys.sendDate]: '2025-04-21',
+      [washKeys.washStatus]: '세탁 완료',
       [washKeys.returnStatus]: '회수 완료',
     },
     {
-      [washKeys.requestUid]: '143143141',
-      [washKeys.requestDate]: '2021-08-31',
-      [washKeys.productUid]: '123123',
-      [washKeys.productName]: '품목명',
-      [washKeys.requestQuantity]: 3,
-      [washKeys.estimatedDate]: '2021-09-01',
-      [washKeys.company]: '요청 업체',
-      [washKeys.deliveryStatus]: '배송 상태',
-      [washKeys.sendDate]: '2021-09-01',
-      [washKeys.washStatus]: '세탁 대기',
+      [washKeys.requestUid]: 'W202501130002',
+      [washKeys.requestDate]: '2025-04-18',
+      [washKeys.productUid]: 'P00000002',
+      [washKeys.productName]: '호텔 침구세트',
+      [washKeys.requestQuantity]: 12,
+      [washKeys.estimatedDate]: '2025-04-22',
+      [washKeys.company]: '롯데호텔 서울',
+      [washKeys.deliveryStatus]: '배송 완료',
+      [washKeys.sendDate]: '2025-04-20',
+      [washKeys.washStatus]: '세탁 완료',
       [washKeys.returnStatus]: '회수 완료',
     },
     {
-      [washKeys.requestUid]: '143143141',
-      [washKeys.requestDate]: '2021-08-31',
-      [washKeys.productUid]: '123123',
-      [washKeys.productName]: '품목명',
-      [washKeys.requestQuantity]: 3,
-      [washKeys.estimatedDate]: '2021-09-01',
-      [washKeys.company]: '요청 업체',
-      [washKeys.deliveryStatus]: '배송 상태',
-      [washKeys.sendDate]: '2021-09-01',
+      [washKeys.requestUid]: 'W202501130003',
+      [washKeys.requestDate]: '2025-04-25',
+      [washKeys.productUid]: 'P00000003',
+      [washKeys.productName]: '식당 앞치마',
+      [washKeys.requestQuantity]: 268,
+      [washKeys.estimatedDate]: '2025-04-30',
+      [washKeys.company]: '아워홈 구내식당',
+      [washKeys.deliveryStatus]: '자체 수거',
+      [washKeys.sendDate]: '-',
       [washKeys.washStatus]: '세탁 대기',
+      [washKeys.returnStatus]: '-',
+    },
+    {
+      [washKeys.requestUid]: 'W202501130004',
+      [washKeys.requestDate]: '2025-04-25',
+      [washKeys.productUid]: 'P00000004',
+      [washKeys.productName]: '산업용 작업복',
+      [washKeys.requestQuantity]: 78,
+      [washKeys.estimatedDate]: '2025-04-30',
+      [washKeys.company]: '현대제철 당진공장',
+      [washKeys.deliveryStatus]: '자체 수거',
+      [washKeys.sendDate]: '-',
+      [washKeys.washStatus]: '세탁 대기',
+      [washKeys.returnStatus]: '-',
+    },
+    {
+      [washKeys.requestUid]: 'W202501130005',
+      [washKeys.requestDate]: '2025-04-25',
+      [washKeys.productUid]: 'P00000005',
+      [washKeys.productName]: '호텔 타월',
+      [washKeys.requestQuantity]: 76,
+      [washKeys.estimatedDate]: '2025-05-01',
+      [washKeys.company]: '웨스틴조선호텔',
+      [washKeys.deliveryStatus]: '자체 수거',
+      [washKeys.sendDate]: '-',
+      [washKeys.washStatus]: '세탁 대기',
+      [washKeys.returnStatus]: '-',
+    },
+    {
+      [washKeys.requestUid]: 'W202501130006',
+      [washKeys.requestDate]: '2025-04-21',
+      [washKeys.productUid]: 'P00000006',
+      [washKeys.productName]: '의료용 커튼',
+      [washKeys.requestQuantity]: 9,
+      [washKeys.estimatedDate]: '2025-04-26',
+      [washKeys.company]: '삼성서울병원',
+      [washKeys.deliveryStatus]: '배송중',
+      [washKeys.sendDate]: '2025-04-25',
+      [washKeys.washStatus]: '세탁 완료',
+      [washKeys.returnStatus]: '-',
+    },
+    {
+      [washKeys.requestUid]: 'W202501130007',
+      [washKeys.requestDate]: '2025-04-10',
+      [washKeys.productUid]: 'P00000007',
+      [washKeys.productName]: '식당 테이블보',
+      [washKeys.requestQuantity]: 42,
+      [washKeys.estimatedDate]: '2025-04-13',
+      [washKeys.company]: '신세계푸드 본사',
+      [washKeys.deliveryStatus]: '배송 완료',
+      [washKeys.sendDate]: '2025-04-12',
+      [washKeys.washStatus]: '세탁 완료',
       [washKeys.returnStatus]: '회수 완료',
     },
     {
-      [washKeys.requestUid]: '143143141',
-      [washKeys.requestDate]: '2021-08-31',
-      [washKeys.productUid]: '123123',
-      [washKeys.productName]: '품목명',
-      [washKeys.requestQuantity]: 3,
-      [washKeys.estimatedDate]: '2021-09-01',
-      [washKeys.company]: '요청 업체',
-      [washKeys.deliveryStatus]: '배송 상태',
-      [washKeys.sendDate]: '2021-09-01',
-      [washKeys.washStatus]: '세탁 대기',
-      [washKeys.returnStatus]: '회수 완료',
+      [washKeys.requestUid]: 'W202501130008',
+      [washKeys.requestDate]: '2025-04-25',
+      [washKeys.productUid]: 'P00000008',
+      [washKeys.productName]: '산업용 장갑',
+      [washKeys.requestQuantity]: 126,
+      [washKeys.estimatedDate]: '2025-05-01',
+      [washKeys.company]: 'LG화학 여수공장',
+      [washKeys.deliveryStatus]: '자체 수거',
+      [washKeys.sendDate]: '-',
+      [washKeys.washStatus]: '세탁중',
+      [washKeys.returnStatus]: '-',
     },
     {
-      [washKeys.requestUid]: '143143141',
-      [washKeys.requestDate]: '2021-08-31',
-      [washKeys.productUid]: '123123',
-      [washKeys.productName]: '품목명',
-      [washKeys.requestQuantity]: 3,
-      [washKeys.estimatedDate]: '2021-09-01',
-      [washKeys.company]: '요청 업체',
-      [washKeys.deliveryStatus]: '배송 상태',
-      [washKeys.sendDate]: '2021-09-01',
-      [washKeys.washStatus]: '세탁 대기',
-      [washKeys.returnStatus]: '회수 완료',
+      [washKeys.requestUid]: 'W202501130009',
+      [washKeys.requestDate]: '2025-04-25',
+      [washKeys.productUid]: 'P00000009',
+      [washKeys.productName]: '호텔 베개커버',
+      [washKeys.requestQuantity]: 211,
+      [washKeys.estimatedDate]: '2025-04-28',
+      [washKeys.company]: '그랜드하얏트호텔',
+      [washKeys.deliveryStatus]: '배송 준비',
+      [washKeys.sendDate]: '2025-04-26',
+      [washKeys.washStatus]: '세탁 완료',
+      [washKeys.returnStatus]: '-',
     },
     {
-      [washKeys.requestUid]: '143143141',
-      [washKeys.requestDate]: '2021-08-31',
-      [washKeys.productUid]: '123123',
-      [washKeys.productName]: '품목명',
-      [washKeys.requestQuantity]: 3,
-      [washKeys.estimatedDate]: '2021-09-01',
-      [washKeys.company]: '요청 업체',
-      [washKeys.deliveryStatus]: '배송 상태',
-      [washKeys.sendDate]: '2021-09-01',
-      [washKeys.washStatus]: '세탁 대기',
-      [washKeys.returnStatus]: '회수 완료',
-    },
-    {
-      [washKeys.requestUid]: '143143141',
-      [washKeys.requestDate]: '2021-08-31',
-      [washKeys.productUid]: '123123',
-      [washKeys.productName]: '품목명',
-      [washKeys.requestQuantity]: 3,
-      [washKeys.estimatedDate]: '2021-09-01',
-      [washKeys.company]: '요청 업체',
-      [washKeys.deliveryStatus]: '배송 상태',
-      [washKeys.sendDate]: '2021-09-01',
-      [washKeys.washStatus]: '세탁 대기',
-      [washKeys.returnStatus]: '회수 완료',
-    },
-    {
-      [washKeys.requestUid]: '143143141',
-      [washKeys.requestDate]: '2021-08-31',
-      [washKeys.productUid]: '123123',
-      [washKeys.productName]: '품목명',
-      [washKeys.requestQuantity]: 3,
-      [washKeys.estimatedDate]: '2021-09-01',
-      [washKeys.company]: '요청 업체',
-      [washKeys.deliveryStatus]: '배송 상태',
-      [washKeys.sendDate]: '2021-09-01',
-      [washKeys.washStatus]: '세탁 대기',
-      [washKeys.returnStatus]: '회수 완료',
-    },
-    {
-      [washKeys.requestUid]: '143143141',
-      [washKeys.requestDate]: '2021-08-31',
-      [washKeys.productUid]: '123123',
-      [washKeys.productName]: '품목명',
-      [washKeys.requestQuantity]: 3,
-      [washKeys.estimatedDate]: '2021-09-01',
-      [washKeys.company]: '요청 업체',
-      [washKeys.deliveryStatus]: '배송 상태',
-      [washKeys.sendDate]: '2021-09-01',
-      [washKeys.washStatus]: '세탁 대기',
-      [washKeys.returnStatus]: '회수 완료',
-    },
-    {
-      [washKeys.requestUid]: '143143141',
-      [washKeys.requestDate]: '2021-08-31',
-      [washKeys.productUid]: '123123',
-      [washKeys.productName]: '품목명',
-      [washKeys.requestQuantity]: 3,
-      [washKeys.estimatedDate]: '2021-09-01',
-      [washKeys.company]: '요청 업체',
-      [washKeys.deliveryStatus]: '배송 상태',
-      [washKeys.sendDate]: '2021-09-01',
-      [washKeys.washStatus]: '세탁 대기',
+      [washKeys.requestUid]: 'W202501130010',
+      [washKeys.requestDate]: '2025-04-11',
+      [washKeys.productUid]: 'P00000010',
+      [washKeys.productName]: '의료용 마스크',
+      [washKeys.requestQuantity]: 526,
+      [washKeys.estimatedDate]: '2025-04-20',
+      [washKeys.company]: '고려대학교병원',
+      [washKeys.deliveryStatus]: '배송 완료',
+      [washKeys.sendDate]: '2025-04-19',
+      [washKeys.washStatus]: '세탁 완료',
       [washKeys.returnStatus]: '회수 완료',
     },
   ]);
@@ -184,7 +211,11 @@ const Order = () => {
               <BoldText>신청 업체</BoldText>
             </Width>
             <Width width={'10rem'}>
-              <SelectBox className="w-100" placeholder="전체" />
+              <SelectBox
+                className="w-100"
+                placeholder="전체"
+                options={companyList}
+              />
             </Width>
           </Flex>
           <Flex gap="0.5rem">
@@ -192,7 +223,7 @@ const Order = () => {
               <BoldText>세탁 배송 상태</BoldText>
             </Width>
             <Width width={'10rem'}>
-              <SelectBox placeholder="전체" />
+              <SelectBox placeholder="전체" options={deliveryStatus} />
             </Width>
           </Flex>
         </Flex>
@@ -233,7 +264,7 @@ const Order = () => {
               <BoldText>회수 여부</BoldText>
             </Width>
             <Width width={'10rem'}>
-              <SelectBox placeholder="전체" />
+              <SelectBox placeholder="전체" options={returnStatus} />
             </Width>
           </Flex>
           <PrimaryButton className={'ms-auto'}>검색</PrimaryButton>
